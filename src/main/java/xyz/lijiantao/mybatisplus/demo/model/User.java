7ded96f2ac869518a5f7cb4bef012201a18432e0
vlijiantao <vlijiantao@foxmail.com>
2018-12-29 11:21:36 +0800

    User

---
+++ src/main/java/xyz/lijiantao/mybatisplus/demo/model/User.java
@@ -0,0 +1,14 @@
+package xyz.lijiantao.mybatisplus.demo.model;
+
+import lombok.Data;
+
+/**
+ * Created by Jiantao Li on 2018/12/29 10:55
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
