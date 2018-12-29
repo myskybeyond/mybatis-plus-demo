1838b4e02743d2ae52e6edfa269260bf5e602ab6
vlijiantao <vlijiantao@foxmail.com>
2018-12-29 11:14:48 +0800

    demo

--- src/test/java/xyz/lijiantao/mybatisplus/demo/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
+++ src/test/java/xyz/lijiantao/mybatisplus/demo/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -1,16 +1,29 @@
 package xyz.lijiantao.mybatisplus.demo.mybatisplusdemo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import xyz.lijiantao.mybatisplus.demo.mapper.UserMapper;
+import xyz.lijiantao.mybatisplus.demo.model.User;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MybatisPlusDemoApplicationTests {
 
+	@Autowired
+    private UserMapper userMapper;
+
 	@Test
 	public void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
 	}
 
 }
