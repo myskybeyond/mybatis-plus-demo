4a93afc34982f3bb706759d07a4ea8d08e19f6cc
vlijiantao <vlijiantao@foxmail.com>
2018-12-29 11:22:59 +0800

    dev version

---
+++ src/main/java/xyz/lijiantao/mybatisplus/demo/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package xyz.lijiantao.mybatisplus.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import xyz.lijiantao.mybatisplus.demo.model.User;
+
+/**
+ * Created by Jiantao Li on 2018/12/29 10:57
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
